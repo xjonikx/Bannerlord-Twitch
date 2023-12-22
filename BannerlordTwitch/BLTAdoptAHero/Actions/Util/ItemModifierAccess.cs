e2c649bd9ff09b86a6198ec217b249573d1f9ac9
Quentin <quentincosnier@free.fr>
2023-12-22 04:22:56 +0100

    Debug Field Acces of Items To DeclaredProperty

--- BannerlordTwitch/BLTAdoptAHero/Actions/Util/ItemModifierAccess.cs
+++ BannerlordTwitch/BLTAdoptAHero/Actions/Util/ItemModifierAccess.cs
@@ -1,4 +1,5 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using HarmonyLib;
 using TaleWorlds.Core;
 using TaleWorlds.Localization;
 
@@ -8,24 +9,27 @@ namespace BLTAdoptAHero.Actions.Util
     {
         public static void SetName(this ItemModifier item, TextObject name) => 
             AccessTools.Property(typeof(ItemModifier), nameof(ItemModifier.Name)).SetValue(item, name);
-        public static void SetDamageModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "Damage").SetValue(item, value);
+        public static void SetDamageModifier(this ItemModifier item, int value)
+        {
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "Damage").SetValue(item, value);
+        }
+
         public static void SetSpeedModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "Speed").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "Speed").SetValue(item, value);
         public static void SetMissileSpeedModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "MissileSpeed").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "MissileSpeed").SetValue(item, value);
         public static void SetArmorModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "Armor").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "Armor").SetValue(item, value);
         public static void SetHitPointsModifier(this ItemModifier item, short value) => 
-            AccessTools.Field(typeof(ItemModifier), "HitPoints").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "HitPoints").SetValue(item, value);
         public static void SetStackCountModifier(this ItemModifier item, short value) => 
-            AccessTools.Field(typeof(ItemModifier), "StackCount").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "StackCount").SetValue(item, value);
         public static void SetMountSpeedModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "MountSpeed").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "MountSpeed").SetValue(item, value);
         public static void SetManeuverModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "Maneuver").SetValue(item, value);
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "Maneuver").SetValue(item, value);
         public static void SetChargeDamageModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "ChargeDamage").SetValue(item, value);
-        public static void SetMountHitPointsModifier(this ItemModifier item, float value) => AccessTools.Field(typeof(ItemModifier), "MountHitPoints").SetValue(item, value);    
+            AccessTools.DeclaredProperty(typeof(ItemModifier), "ChargeDamage").SetValue(item, value);
+        public static void SetMountHitPointsModifier(this ItemModifier item, float value) => AccessTools.DeclaredProperty(typeof(ItemModifier), "MountHitPoints").SetValue(item, value);    
     }
 }
