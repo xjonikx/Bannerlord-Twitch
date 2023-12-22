14273e160abbb8fd5735b0e6a7e1560eb72bba5f
Quentin <quentincosnier@free.fr>
2023-12-22 03:01:49 +0100

    Debug Harmony item fields Patching

--- BannerlordTwitch/BLTAdoptAHero/Actions/Util/ItemModifierAccess.cs
+++ BannerlordTwitch/BLTAdoptAHero/Actions/Util/ItemModifierAccess.cs
@@ -9,23 +9,23 @@ namespace BLTAdoptAHero.Actions.Util
         public static void SetName(this ItemModifier item, TextObject name) => 
             AccessTools.Property(typeof(ItemModifier), nameof(ItemModifier.Name)).SetValue(item, name);
         public static void SetDamageModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "_damage").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "Damage").SetValue(item, value);
         public static void SetSpeedModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "_speed").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "Speed").SetValue(item, value);
         public static void SetMissileSpeedModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "_missileSpeed").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "MissileSpeed").SetValue(item, value);
         public static void SetArmorModifier(this ItemModifier item, int value) => 
-            AccessTools.Field(typeof(ItemModifier), "_armor").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "Armor").SetValue(item, value);
         public static void SetHitPointsModifier(this ItemModifier item, short value) => 
-            AccessTools.Field(typeof(ItemModifier), "_hitPoints").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "HitPoints").SetValue(item, value);
         public static void SetStackCountModifier(this ItemModifier item, short value) => 
-            AccessTools.Field(typeof(ItemModifier), "_stackCount").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "StackCount").SetValue(item, value);
         public static void SetMountSpeedModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "_mountSpeed").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "MountSpeed").SetValue(item, value);
         public static void SetManeuverModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "_maneuver").SetValue(item, value);
+            AccessTools.Field(typeof(ItemModifier), "Maneuver").SetValue(item, value);
         public static void SetChargeDamageModifier(this ItemModifier item, float value) => 
-            AccessTools.Field(typeof(ItemModifier), "_chargeDamage").SetValue(item, value);
-        public static void SetMountHitPointsModifier(this ItemModifier item, float value) => AccessTools.Field(typeof(ItemModifier), "_mountHitPoints").SetValue(item, value);    
+            AccessTools.Field(typeof(ItemModifier), "ChargeDamage").SetValue(item, value);
+        public static void SetMountHitPointsModifier(this ItemModifier item, float value) => AccessTools.Field(typeof(ItemModifier), "MountHitPoints").SetValue(item, value);    
     }
 }
