1e50156a59de913831ea4f40321323f512e53a1f
Quentin <quentincosnier@free.fr>
2024-08-19 20:26:15 +0200

    1.2.11

--- BannerlordTwitch/BannerlordTwitch/BLTModule.cs
+++ BannerlordTwitch/BannerlordTwitch/BLTModule.cs
@@ -29,7 +29,7 @@ namespace BannerlordTwitch
 		[DllImport("user32.dll")]
 		private static extern int SetWindowText(IntPtr hWnd, string text);
 
-		private const string ExpectedVersion = "v1.2.10";
+		private const string ExpectedVersion = "v1.2.11";
 
 		static BLTModule()
 		{
