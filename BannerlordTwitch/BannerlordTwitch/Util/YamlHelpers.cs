bc20c2cec82e7194c0e94c4e611f207e5f1b44e6
jazz-ttv <jazz.ttv@mail.com>
2025-01-06 19:10:04 -0600

    Update all packages Changes for YAML package update

--- BannerlordTwitch/BannerlordTwitch/Util/YamlHelpers.cs
+++ BannerlordTwitch/BannerlordTwitch/Util/YamlHelpers.cs
@@ -19,17 +19,18 @@ namespace BannerlordTwitch.Util
         {
             public bool Accepts(Type type) => type == typeof(LocString);
 
-            public object ReadYaml(IParser parser, Type type)
+            public object ReadYaml(IParser parser, Type type, ObjectDeserializer deserializer)
             {
                 return new LocString(parser.Consume<Scalar>().Value);
             }
 
-            public void WriteYaml(IEmitter emitter, object value, Type type)
+            public void WriteYaml(IEmitter emitter, object value, Type type, ObjectSerializer serializer)
             {
                 emitter.Emit(new Scalar((value as LocString)?.Value ?? string.Empty));
             }
         }
-        
+
+
         private class SortedTypeInspector : TypeInspectorSkeleton
         {
             private readonly ITypeInspector innerTypeInspector;
@@ -43,8 +44,18 @@ namespace BannerlordTwitch.Util
             {
                 return innerTypeInspector.GetProperties(type, container).OrderBy(x => x.Name);
             }
+
+            public override string GetEnumName(Type enumType, string name)
+            {
+                return innerTypeInspector.GetEnumName(enumType, name);
+            }
+
+            public override string GetEnumValue(object enumValue)
+            {
+                return innerTypeInspector.GetEnumValue(enumValue);
+            }
         }
-        
+
         public static string Serialize(object obj) => CreateDefaultSerializer().Serialize(obj);
         public static object Deserialize(string str, Type type) => CreateDefaultDeserializer().Deserialize(str, type);
         public static string SerializeUntagged(object obj) => CreateUntaggedSerializer().Serialize(obj);
